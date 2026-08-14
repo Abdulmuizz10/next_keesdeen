250021314b11cfb433b2c72fba090a478f7d9fa8
Your Name <harrismuizz10@gmail.com>
2026-08-14 04:45:13 +0100

    seventh commit

--- src/app/(storefront)/collections/page.tsx
+++ src/app/(storefront)/collections/page.tsx
@@ -67,7 +67,9 @@ export default async function CollectionsIndexPage() {
                   alt={col.name}
                   width={100}
                   height={100}
+                  sizes="full"
                   fill
+                  priority
                   className="h-full w-full object-cover transition-transform duration-1000 ease-out-expo group-hover:scale-105"
                 />
               ) : (
